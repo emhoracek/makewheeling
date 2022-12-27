3dd32a11ffa876f894cb1213212f577e36136edf
Libby Horacek <libby@positiondev.com>
2022-12-27 07:14:20 -0500

    Update homepage

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -24,53 +24,6 @@ export default function Home() {
         </p>
       </div>
 
-      <div class="coffee-and-tech">
-        <h2>Coffee &amp; Tech</h2>
-
-        <img src="https://cdn.glitch.global/a66f4dd6-ce3f-45aa-a4fe-67fb736c819b/309309552_10159902218436664_7776853586986855260_n.jpg?v=1664397802119"
-             className="flyer"
-             alt="A flyer for coffee & tech" />
-        <p>
-          <strong>Coffee &amp; Tech</strong> is Make Wheeling's first program.
-          It's a meet-up for tech professionals and anyone who is interested in
-          learning more about technology.
-        </p>
-
-        <p>
-          <strong>Coffee &amp; Tech</strong> is structured around the
-          the <a href="https://agilecoffee.com/leancoffee/">Lean Coffee</a> meeting facilitation technique. We'll share the ideas and issues that
-          are on our minds, vote on topics to discuss, and discuss the most
-          popular topics one-by-one. All while having some delicious morning
-          beverages!
-        </p>
-
-        <h3>
-          Next <strong>and last</strong> meeting: Tuesday, December 27, 2022
-        </h3>
-
-        <p>
-          Location:&nbsp;
-          <strong>
-            <a href="https://www.facebook.com/mugshots304/">Mugshots</a>
-          </strong> in downtown Wheeling.
-          <br />
-          Start: <strong>7:30am</strong>
-          <span class="small"> (but if you can't make it that early, just show up whenever)
-          </span>
-          <br />
-          End: <strong>9am</strong>
-          <span class="small"> (but if you have to leave before then, no problem)
-          </span>
-          <br />
-          Cost: <strong>free</strong>
-          <span class="small"> (but if you can, bring a few bucks for coffee)
-          </span>
-        </p>
-
-        <p><a href="https://cdn.glitch.global/a66f4dd6-ce3f-45aa-a4fe-67fb736c819b/Screen%20Shot%202022-09-28%20at%204.32.26%20PM.png?v=1664397155663">
-          Download a flyer to share!</a></p>
-      </div>
-
       <div class="faq">
         <h2>Frequently Asked Questions</h2>
 
